22ebb69fdabbdb6af18420a938da38200cc4aac1
Californ1a <rk_california@yahoo.com>
2023-10-21 01:27:46 -0400

    add tooltips to header buttons

--- frontend-client/src/components/inventory-list/HeaderPanel.vue
+++ frontend-client/src/components/inventory-list/HeaderPanel.vue
@@ -2,12 +2,12 @@
   <div class="flex flex-wrap align-items-center justify-content-between gap-2">
     <span class="text-xl text-900 font-bold">{{ name }}</span>
     <div class="right-header-buttons">
-      <Button type="button" icon="pi pi-filter-slash" label="Clear" outlined @click="clearFilters" />
+      <Button type="button" icon="pi pi-filter-slash" label="Default" outlined @click="clearFilters" v-tooltip.top="'Reset all filters to default'" />
       <span class="p-input-icon-left">
         <i class="pi pi-search" />
         <InputText :modelValue="filters['global'].value" @update:modelValue="$emit('inputUpdate', $event)" placeholder="Keyword Search" />
       </span>
-      <Button icon="pi pi-refresh" raised @click="fetchData" severity="secondary" />
+      <Button icon="pi pi-refresh" raised @click="fetchData" severity="secondary" v-tooltip.top="'Reload data'" />
     </div>
   </div>
 </template>
