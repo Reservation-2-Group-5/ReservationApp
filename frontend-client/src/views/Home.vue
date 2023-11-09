e34f6d8ce4591bbe2861f2ead75d39e3fdc7be77
ovenuti <ovenuti@students.kennesaw.edu>
2023-11-08 20:54:42 -0500

    Home.vue code added, 2 pictures added to Public Folder

--- frontend-client/src/views/Home.vue
+++ frontend-client/src/views/Home.vue
@@ -1,20 +1,25 @@
 <template>
-  <Card class="card">
-    <template #title>
-      Home
-    </template>
-    <template #content>
-      <p>Form to reserve an inventory item or campus space.</p>
-    </template>
-  </Card>
+    <Card class="card">
+        <template #title>
+            Welcome to the CCSE Reservation Application
+        </template>
+        <template #content>
+            <div class="block">
+                <img src='KSUCCSEpic.jpg' alt="KSU Picture" />
+                <p> Kennesaw State University's College of Computing and Software Engineering is one of the best in the country, with each of its degree programs being recognized nationally. We offer both undergraduate and graduate degrees in Computer Science, Information Technology, Software Engineering and Computer Game Design and Development.</p>
+            </div>
+        </template>
+    </Card>
 </template>
-
 <script setup>
-import Card from 'primevue/card';
+    import Card from 'primevue/card'
 </script>
-
 <style scoped>
-.card {
-  height: 100%;
-}
+    .card {
+        height: 100%;
+    }
+    .block{
+        display:flex;
+        gap:50px;
+    }
 </style>
