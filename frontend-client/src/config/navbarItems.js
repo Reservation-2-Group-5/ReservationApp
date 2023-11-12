744db95d3a541b08cfc7fb83fbdd9d3cc91f9c5a
Californ1a <rk_california@yahoo.com>
2023-11-12 00:41:07 -0500

    start sending real db requests

--- frontend-client/src/config/navbarItems.js
+++ frontend-client/src/config/navbarItems.js
@@ -2,24 +2,30 @@ export default [{
   label: 'Home',
   icon: 'pi pi-fw pi-home',
   route: '/',
+  auth: false,
 }, {
   label: 'Inventory',
   icon: 'pi pi-fw pi-list',
   route: '/inventory',
+  auth: 'user',
 }, {
   label: 'Rooms',
   icon: 'pi pi-fw pi-building',
   route: '/rooms',
+  auth: 'user',
 }, {
   label: 'Login',
   icon: 'pi pi-fw pi-user',
   route: '/login',
+  auth: 'guest',
 }, {
   label: 'Admin',
   icon: 'pi pi-fw pi-wrench',
   route: '/admin',
+  auth: 'admin',
 }, {
   label: 'Logout',
   icon: 'pi pi-fw pi-user',
   route: '/logout',
+  auth: 'user',
 }];
--- frontend-client/src/store.js
+++ frontend-client/src/store.js
@@ -18,6 +18,22 @@ const fixDate = (date) => {
   return date;
 };
 
+const formatUserData = (data) => {
+  const formattedData = [];
+  for (const user of data) {
+    const newUser = {};
+    newUser.netId = user.NetID;
+    newUser.name = user.Name;
+    newUser.email = user.Email;
+    newUser.isFaculty = user.Is_Faculty;
+    newUser.isStudent = user.Is_Student;
+    newUser.isAdmin = user.Is_Admin;
+
+    formattedData.push(newUser);
+  }
+  return formattedData;
+};
+
 const formatInventoryData = (data) => {
   const formattedData = [];
   for (const item of data) {
@@ -77,6 +93,7 @@ const formatDeviceReservationData = (data) => {
     newRes.warrantyExpiration = fixDate(res.Warranty_EXP);
     newRes.available = (res.Available) ? 'available' : 'unavailable';
 
+    newRes.id = res.id;
     newRes.requestedEndDate = fixDate(res.End_Date);
     newRes.requestedBy = res.Name;
     newRes.reqNetId = res.NetID;
@@ -104,6 +121,7 @@ const formatRoomReservationData = (data) => {
     newRes.maxOccupancy = res.Max_Occupancy;
     newRes.type = (res.Is_Office) ? 'Office' : 'Conference';
 
+    newRes.id = res.id;
     newRes.requestedBy = res.Name;
     newRes.reqNetId = res.NetID;
     newRes.requestedOnDate = fixDate(res.Request_Date);
@@ -114,20 +132,48 @@ const formatRoomReservationData = (data) => {
 };
 
 export const useUserStore = defineStore('user', () => {
+  const users = ref([]);
   const user = ref(null);
 
   const setUser = (newUser) => {
     user.value = newUser;
   };
 
+  const setUsers = (newUsers) => {
+    users.value = formatUserData(newUsers);
+  };
+
   const isLoggedIn = computed(() => !!user.value);
-  const isAdmin = computed(() => user.value?.role === 'admin');
+  const isAdmin = computed(() => user.value?.isAdmin);
+
+  async function fetchUsers() {
+    try {
+      const response = await fetch(`${API}/users`);
+      const json = await response.json();
+      setUsers(json);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // TODO: implement once we have netid system
+  async function login() {
+    if (user.value) return; // don't login if already logged in
+    if (!users.value.length) {
+      await fetchUsers();
+    }
+    // FIXME: pick a random user with admin role for now
+    const adminUsers = users.value.filter((u) => u.isAdmin);
+    const randomAdmin = adminUsers[Math.floor(Math.random() * adminUsers.length)];
+    setUser(randomAdmin);
+  }
 
   return {
     user,
     setUser,
     isLoggedIn,
     isAdmin,
+    login,
   };
 });
 
@@ -139,33 +185,17 @@ export const useInventoryStore = defineStore('inventory', () => {
   };
 
   const fetchInventory = async () => {
-    // const location = (isDev) ? 'realInventoryTestData.json' : 'db';
-    // const location = 'api/v1/devices';
-    const deviceResLocation = `${API}/devices`;
-    const testDataLocation = 'realInventoryTestData.json';
-    if (isDev) {
-      // simulate a fetch delay
-      await sleep(1000);
-    }
     try {
       // Check if /api is accessible
       const useApi = await apiAccessible();
-      const selectedLocation = (useApi) ? deviceResLocation : testDataLocation;
+      if (!useApi) return; // don't fetch if api is not accessible
 
       // Fetch device reservations
-      const response = await fetch(selectedLocation);
+      const response = await fetch(`${API}/devices`);
       const json = await response.json();
       setInventory(json);
     } catch (err) {
       console.error(err);
-    }
-  };
-
-  const setItemImg = (id, img) => {
-    for (const item of inventory.value) {
-      if (item.id === id) {
-        item.img = img;
-      }
     }
   };
 
@@ -178,7 +208,6 @@ export const useInventoryStore = defineStore('inventory', () => {
   return {
     inventory,
     setInventory,
-    setItemImg,
     fetchInventory,
     fetchAll,
     getAll,
@@ -190,47 +219,16 @@ export const useDeviceReservationStore = defineStore('deviceReservation', () => 
 
   const setReservations = (newReservations) => {
     deviceReservations.value = formatDeviceReservationData(newReservations);
-    let id = 0;
-
-    const requestsExist = deviceReservations.value.filter((res) => !!res.requestedBy);
-    if (requestsExist.length) return; // don't add fake data if real data exists
-
-    for (const item of deviceReservations.value) {
-      // Add reservation data to devices - simulate db JOIN query
-      item.id = id;
-      id += 1;
-      // random requester
-      item.requestedBy = `${
-        ['John', 'Jane', 'Joe', 'Jill', 'Jack'][Math.floor(Math.random() * 5)]
-      } ${
-        ['Smith', 'Doe', 'Johnson', 'Williams', 'Brown'][Math.floor(Math.random() * 5)]
-      }`;
-      const firstInitial = item.requestedBy[0].toLowerCase();
-      const lastName = item.requestedBy.split(' ')[1].toLowerCase();
-      item.reqNetId = `${firstInitial}${lastName}`;
-      // random date between now and 2 weeks from now
-      item.requestedOnDate = new Date(Date.now() + Math.floor(Math.random() * 12096e5));
-      item.requestedStartDate = new Date(item.requestedOnDate);
-      item.requestedEndDate = new Date(item.requestedOnDate.getTime() + 2592e6);
-    }
   };
 
   const fetchReservations = async () => {
-    // const location = (isDev) ? 'realInventoryTestData.json' : 'db';
-    // const location = 'api/v1/device-res';
-    const deviceResLocation = `${API}/device-res`;
-    const testDataLocation = 'realInventoryTestData.json';
-    if (isDev) {
-      // simulate a fetch delay
-      await sleep(1000);
-    }
     try {
       // Check if /api is accessible
       const useApi = await apiAccessible();
-      const selectedLocation = (useApi) ? deviceResLocation : testDataLocation;
+      if (!useApi) return; // don't fetch if api is not accessible
 
       // Fetch device reservations
-      const response = await fetch(selectedLocation);
+      const response = await fetch(`${API}/device-res`);
       const json = await response.json();
 
       setReservations(json);
@@ -239,25 +237,65 @@ export const useDeviceReservationStore = defineStore('deviceReservation', () => 
     }
   };
 
-  const setRequest = (id, to) => {
-    for (const reservation of deviceReservations.value) {
-      if (reservation.id === id) {
-        reservation.approved = to;
-        // remove the request from the list
-        deviceReservations.value = deviceReservations.value.filter(
-          (res) => res.tag !== reservation.tag,
-        );
-      }
+  // update the request in the db
+  const editRequest = async (res, to) => {
+    const body = {
+      status: (to) ? 'approved' : 'denied',
+      NetID: res.reqNetId,
+      Name: res.requestedBy,
+    };
+    const response = await fetch(`${API}/device-res/${res.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    const json = await response.json();
+    console.log(json);
+    if (response.status !== 200) {
+      throw new Error(json.message);
     }
   };
 
-  const approveRequest = (id) => {
-    setRequest(id, true);
+  // locally update the request
+  const setRequest = async (res, to) => {
+    for (const reservation of deviceReservations.value) {
+      if (reservation.id === res.id) {
+        reservation.approved = to;
+        // remove the request from the list locally
+        deviceReservations.value = deviceReservations.value.filter(
+          (fRes) => fRes.tag !== reservation.tag,
+        );
+      }
+    }
+    await editRequest(res, to);
   };
 
-  const denyRequest = (id) => {
-    setRequest(id, false);
+  const createReservation = async (res) => {
+    const body = {
+      Tag: res.tag,
+      NetID: res.reqNetId,
+      Start_Date: res.requestedStartDate,
+      End_Date: res.requestedEndDate,
+    };
+    const response = await fetch(`${API}/device-res`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    const json = await response.json();
+    console.log(json);
+    if (response.status !== 200) {
+      throw new Error('Error creating reservation');
+    }
   };
+
+  const approveRequest = (res) => setRequest(res, true);
+
+  const denyRequest = (res) => setRequest(res, false);
 
   const fetchAll = async () => {
     await fetchReservations();
@@ -273,6 +311,7 @@ export const useDeviceReservationStore = defineStore('deviceReservation', () => 
     denyRequest,
     fetchAll,
     getAll,
+    createReservation,
   };
 });
 
