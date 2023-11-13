cc7bcad65d05e5cd93174f40474a756968fbeded
Californ1a <rk_california@yahoo.com>
2023-11-12 19:19:42 -0500

    stylistic

--- frontend-client/src/views/Home.vue
+++ frontend-client/src/views/Home.vue
@@ -1,25 +1,29 @@
 <template>
-    <Card class="card">
-        <template #title>
-            Welcome to the CCSE Reservation Application
-        </template>
-        <template #content>
-            <div class="block">
-                <img src='KSUCCSEpic.jpg' alt="KSU Picture" />
-                <p> Kennesaw State University's College of Computing and Software Engineering is one of the best in the country, with each of its degree programs being recognized nationally. We offer both undergraduate and graduate degrees in Computer Science, Information Technology, Software Engineering and Computer Game Design and Development.</p>
-            </div>
-        </template>
-    </Card>
+  <Card class="card">
+    <template #title>
+      Welcome to the CCSE Reservation Application
+    </template>
+    <template #content>
+      <div class="block">
+        <img src='KSUCCSEpic.jpg' alt="KSU Picture" />
+        <!-- eslint-disable-next-line vue/max-len -->
+        <p>Kennesaw State University's College of Computing and Software Engineering is one of the best in the country, with each of its degree programs being recognized nationally. We offer both undergraduate and graduate degrees in Computer Science, Information Technology, Software Engineering and Computer Game Design and Development.</p>
+      </div>
+    </template>
+  </Card>
 </template>
+
 <script setup>
-    import Card from 'primevue/card'
+import Card from 'primevue/card';
 </script>
+
 <style scoped>
-    .card {
-        height: 100%;
-    }
-    .block{
-        display:flex;
-        gap:50px;
-    }
+.card {
+  height: 100%;
+}
+
+.block {
+  display: flex;
+  gap: 50px;
+}
 </style>
