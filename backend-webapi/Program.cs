d7388b6748401054bb50fc2add9a5fa982b24e21
Dina Levieva <levieva.dina@gmail.com>
2023-11-14 14:28:53 -0500

    database connection

--- backend-webapi/Program.cs
+++ backend-webapi/Program.cs
@@ -12,25 +12,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 
-var dbPath = Path.Combine(builder.Environment.ContentRootPath, "dev.sqlite3");
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite($"Data Source={dbPath}"));
+    var dbPath = Path.Combine(builder.Environment.ContentRootPath, "dev.sqlite3");
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlite($"Data Source={dbPath}"));
 
 
-builder.Services.AddScoped<IDeviceService, DeviceService>();
-builder.Services.AddScoped<IDeviceResService, DeviceResService>();
-builder.Services.AddScoped<IRoomResService, RoomResService>();
+    builder.Services.AddScoped<IDeviceService, DeviceService>();
+    builder.Services.AddScoped<IDeviceResService, DeviceResService>();
+    builder.Services.AddScoped<IRoomResService, RoomResService>();
 
 
-var app = builder.Build();
+    var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
