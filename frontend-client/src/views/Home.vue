0c59af287e45bc40122867ddc9d5767583a912c4
ovenuti <ovenuti@students.kennesaw.edu>
2023-11-16 10:20:27 -0500

    change to home.vue page

--- frontend-client/src/views/Home.vue
+++ frontend-client/src/views/Home.vue
@@ -7,6 +7,7 @@
             <div class="block">
                 <img src='KSUCCSEpic.jpg' alt="KSU Picture" />
                 <p> Kennesaw State University's College of Computing and Software Engineering is one of the best in the country, with each of its degree programs being recognized nationally. We offer both undergraduate and graduate degrees in Computer Science, Information Technology, Software Engineering and Computer Game Design and Development.</p>
+                <p>Group study rooms at Johnson and Sturgis Library are provided to give students a place to engage in collaborative and cooperative study in small groups. Permanent assignment of rooms are not made to any particular academic group or social organization.</p>
             </div>
         </template>
     </Card>
